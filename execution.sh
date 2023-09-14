63b771da617d65e7328831c6105196b3d345a77b
anryu15 <anryu.15@gmail.com>
2023-09-14 03:18:56 +0000

    add bash file

--- execution.sh
+++ execution.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/bin/bash 
 yes | apt-get install mecab libmecab-dev mecab-ipadic-utf8
+
 apt install -y --reinstall build-essential
 git clone https://github.com/neologd/mecab-ipadic-neologd.git
+
 ./mecab-ipadic-neologd/bin/install-mecab-ipadic-neologd -n -p /usr/share/mecab/dic/mecab-ipadic-neologd
