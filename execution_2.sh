acaa36449340865fd65896aa188868c514600f66
anryu15 <anryu.15@gmail.com>
2023-09-14 04:05:20 +0000

    fix bash file

--- execution_2.sh
+++ execution_2.sh
@@ -1,9 +1,3 @@
 sudo cp /etc/mecabrc /usr/local/etc/
-<<<<<<< HEAD
 cp -r /usr/share/mecab /usr/local/lib/
-=======
-
-cp -r /usr/share/mecab /usr/local/lib/
-
->>>>>>> e75d61d4c6ece0ea53e1ad9100a65bc72f38b8aa
 cp -r /usr/local/lib/mecab/dic/mecab-ipadic-neologd/ /var/lib/mecab/dic/
